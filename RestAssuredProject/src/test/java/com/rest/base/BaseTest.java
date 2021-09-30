0a6513eef10f9bff8dec9a43d31179b08b4e4407
Patil_Ys <Patil_Ys@Yogesh-Pt.India.XoriantCorp.com>
2021-09-30 13:07:44 +0530

    Log4j commit new version 2.13.2 added.

--- RestAssuredProject/src/test/java/com/rest/base/BaseTest.java
+++ RestAssuredProject/src/test/java/com/rest/base/BaseTest.java
@@ -7,20 +7,17 @@
 
 package com.rest.base;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.testng.annotations.BeforeClass;
-
-import com.rest.apiconstants.APIEndpointConstants;
 
 public class BaseTest {
 	protected Logger logger;
 
 	@BeforeClass
 	public void setup() {
-		logger = Logger.getLogger("Practise API");	  // added Logger
-		PropertyConfigurator.configure(APIEndpointConstants.LOG4J_FILE_PATH); 	// added logger
-		logger.setLevel(Level.DEBUG);
+		logger = (Logger) LogManager.getLogger(BaseTest.class);	  // added Logger
+		// PropertyConfigurator.configure(APIEndpointConstants.LOG4J_FILE_PATH); 	// added logger
+		// logger.setLevel(Level.DEBUG);
 	}
 }
